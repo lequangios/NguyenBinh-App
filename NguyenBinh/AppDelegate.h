738372c2fabb51385ee363ec0e295996d673c28a
VietQuang <macbookpro@MacBookPros-MacBook-Pro.local>
2012-12-09 21:46:03 +0700

    Initial Commit

---
+++ NguyenBinh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NguyenBinh
+//
+//  Created by VietQuang on 12/9/12.
+//  Copyright (c) 2012 VietQuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
